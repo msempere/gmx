262c3c8deadec9f2c2ae618800948268ca7e9e08
MSempere <msempere@gmx.com>
2016-01-05 19:09:18 +0000

    Added forgotten mailbox

--- gmx/gmx.py
+++ gmx/gmx.py
@@ -33,6 +33,16 @@ class Gmx():
                 mailbox.external_name = mailbox_name
                 self.mailboxes[mailbox_name] = mailbox
 
+    def mailbox(self, mailbox_name):
+        if mailbox_name not in self.mailboxes:
+            mailbox_name = encode_utf7(mailbox_name)
+            mailbox = self.mailboxes.get(mailbox_name)
+
+        if mailbox and not self.current_mailbox == mailbox_name:
+            self.use_mailbox(mailbox_name)
+
+        return mailbox
+
     def login(self, username, password):
         self.username = username
         self.password = password
